b9f2b931c233bada6e012fdd8f9bd7fbd30c3b57
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-23 00:25:46 +0300

    First React App with JSX

---
+++ lesson1/task3/webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'review_build'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
