4793d855650f6b69b16d76f8e344eb16004c1ccf
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-23 00:26:32 +0300

    First React App with JSX

---
+++ lesson1/task4/src/index.js
@@ -0,0 +1,19 @@
+// First React App with JSX
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const rootElement = document.querySelector('#root');
+
+const renderGreeting = elem => {
+  const containerElem = (
+    <div className="greeting">
+      <div className="greeting__title">Hello, world!</div>
+      <div className="greeting__text">I'm learning React</div>
+    </div>
+  );
+
+  ReactDOM.render(containerElem, elem);
+};
+
+renderGreeting(rootElement);
