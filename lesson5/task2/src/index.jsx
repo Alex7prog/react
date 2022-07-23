d1d57226ab8629aa8cdeb344423dd6f072d35a27
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-24 02:54:52 +0300

    Good button - event.target

---
+++ lesson5/task2/src/index.jsx
@@ -0,0 +1,11 @@
+// Good button - event.target
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.scss';
+import GoodButton from './GoodButton';
+
+const rootElement = document.querySelector('#root');
+const root = createRoot(rootElement);
+
+root.render(<GoodButton />);
