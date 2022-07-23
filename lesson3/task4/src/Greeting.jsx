d41c3947f034d9f44ca3ff6d6ec2e3015f7d145f
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-23 16:32:33 +0300

    Greeting

---
+++ lesson3/task4/src/Greeting.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const getAge = birthDate => {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+
+  let age = currentYear - birthDate.getFullYear();
+
+  if (age <= 0) {
+    return 0;
+  }
+
+  const birthDateCurrentYear = new Date(birthDate).setFullYear(currentYear);
+
+  if (birthDateCurrentYear > currentDate) {
+    age -= 1;
+  }
+
+  return age;
+};
+
+const Greeting = props => {
+  return (
+    <div className="greeting">
+      {`My name is ${props.user.name} ${props.user.lastName}. I'm 
+      ${getAge(props.user.birthDate)} years old`}
+    </div>
+  );
+};
+
+//
+
+export default Greeting;
---
+++ lesson3/task4/src/index.jsx
@@ -0,0 +1,17 @@
+// Greeting
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.scss';
+import Greeting from './Greeting';
+
+const rootElement = document.querySelector('#root');
+const root = createRoot(rootElement);
+
+const user = {
+  name: 'John',
+  lastName: 'Doe',
+  birthDate: new Date('2001-01-01T11:11:11.819Z'),
+};
+
+root.render(<Greeting user={user} />);
