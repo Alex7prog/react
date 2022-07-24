e9d7b088d8cf20f1f213ee9dbb428de3c54b4d1b
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-24 20:31:53 +0300

    Color picker

--- lesson5/task6/src/ColorPicker.jsx
+++ lesson5/task6/src/ColorPicker.jsx
@@ -9,18 +9,24 @@ class ColorPicker extends Component {
     };
   }
 
-  getColorElem = classElem => {
-    const posStart = classElem.lastIndexOf('_') + 1;
+  // getColorElem = classElem => {
+  //   const posStart = classElem.lastIndexOf('_') + 1;
 
-    return classElem[posStart].toUpperCase() + classElem.substring(posStart + 1, classElem.length);
-  };
+  //   return classElem[posStart].toUpperCase() + classElem.substring(posStart + 1, classElem.length);
+  // };
 
-  setTitleText = e => {
-    if (e.target.classList.contains('picker__button')) {
-      this.setState({
-        colorText: this.getColorElem(e.target.className),
-      });
-    }
+  // setTitleText = e => {
+  //   if (e.target.classList.contains('picker__button')) {
+  //     this.setState({
+  //       colorText: this.getColorElem(e.target.className),
+  //     });
+  //   }
+  // };
+
+  setTitleText = color => {
+    this.setState({
+      colorText: color,
+    });
   };
 
   setTitleClear = () => {
@@ -31,12 +37,25 @@ class ColorPicker extends Component {
 
   render() {
     return (
-      <div onMouseOver={this.setTitleText} onMouseOut={this.setTitleClear}>
+      // <div onMouseOver={this.setTitleText} onMouseOut={this.setTitleClear}>
+      <div>
         <div className="picker__title">{this.state.colorText}</div>
         <div>
-          <button className="picker__button picker__button_coral"></button>
-          <button className="picker__button picker__button_aqua"></button>
-          <button className="picker__button picker__button_bisque"></button>
+          <button
+            className="picker__button picker__button_coral"
+            onMouseOver={() => this.setTitleText('Coral')}
+            onMouseOut={this.setTitleClear}
+          ></button>
+          <button
+            className="picker__button picker__button_aqua"
+            onMouseOver={() => this.setTitleText('Aqua')}
+            onMouseOut={this.setTitleClear}
+          ></button>
+          <button
+            className="picker__button picker__button_bisque"
+            onMouseOver={() => this.setTitleText('Bisque')}
+            onMouseOut={this.setTitleClear}
+          ></button>
         </div>
       </div>
     );
