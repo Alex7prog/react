16f814664bb91f874313d7ea6bfee423c311a6da
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-25 12:26:23 +0300

    Conditional rendering with if

---
+++ lesson6/task1/src/index.jsx
@@ -0,0 +1,11 @@
+// Conditional rendering with if
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.scss';
+import Auth from './Auth';
+
+const rootElement = document.querySelector('#root');
+const root = createRoot(rootElement);
+
+root.render(<Auth />);
