b3f144e0cbce2e2fff7d0fe493625dea2216f06b
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-27 00:53:58 +0300

    Event subscription

--- lesson8/task3/src/index.jsx
+++ lesson8/task3/src/index.jsx
@@ -2,9 +2,9 @@
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Dimension from '../../task3/src/Dimension';
+import Dimensions from '../../task3/src/Dimensions';
 import './index.scss';
 
 const root = createRoot(document.querySelector('#root'));
 
-root.render(<Dimension />);
+root.render(<Dimensions />);
