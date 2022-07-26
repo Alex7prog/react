1ba471076fcc6c0a488a81832e4fda58a33b0ebd
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-26 23:38:58 +0300

    Lifecycle methods

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -7,12 +7,12 @@ class Life extends Component {
   }
 
   componentDidMount() {
-    console.log('componentDidMount: Api calls, subscriptions');
+    console.log('componentDidMount: API calls, subscriptions');
   }
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
-    return nextProps.number % 2;
+    return true;
   }
 
   componentDidUpdate(prevProps, prevState) {
