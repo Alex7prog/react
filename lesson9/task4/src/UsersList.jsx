9601ed2dc2c2b04e29393306e9795cbbe067c953
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-27 19:12:26 +0300

    User list with filtering

---
+++ lesson9/task4/src/UsersList.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Filter from './Filter';
+import User from './User';
+
+class UsersList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+      count: '',
+    };
+  }
+
+  // handleNextPage = () => {
+  //   this.setState({
+  //     page: this.state.page + 1,
+  //   });
+  // };
+
+  handleFilter = e => {
+    e.preventDefault();
+    this.setState({
+      filterText: e.target.value,
+    });
+  };
+
+  render() {
+    let usersList = this.props.users
+      .slice()
+      .filter(user => user.name.toUpperCase().includes(this.state.filterText.toUpperCase()));
+
+    return (
+      <div>
+        <Filter
+          filterText={this.state.filterText}
+          count={usersList.length}
+          onChange={this.handleFilter}
+        />
+        <ul className="users">
+          {usersList.map(user => (
+            <User key={user.id} {...user} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default UsersList;
