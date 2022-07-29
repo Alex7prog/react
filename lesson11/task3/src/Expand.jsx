c0d8769c0f7d14e6c7d5d45baf40e049ea27e62b
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-29 23:27:37 +0300

    Expanded Component

--- lesson11/task3/src/Expand.jsx
+++ lesson11/task3/src/Expand.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import '@fortawesome/react-fontawesome';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
-import 'font-awesome/css/font-awesome.min.css';
+// import 'font-awesome/css/font-awesome.min.css';
 // import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 class Expand extends React.Component {
@@ -24,17 +26,16 @@ class Expand extends React.Component {
         <div className="expand__header">
           <span className="expand__title">{this.props.title}</span>
           <button className="expand__toggle-btn" onClick={this.handleToggle}>
-            {this.state.isHide ? (
+            {/* {this.state.isHide ? (
               <i className="fas fa-chevron-down" />
             ) : (
               <i className="fas fa-chevron-up" />
-            )}
-
-            {/* {this.state.isHide ? (
+            )} */}
+            {this.state.isHide ? (
               <FontAwesomeIcon icon={faChevronDown} />
             ) : (
               <FontAwesomeIcon icon={faChevronUp} />
-            )} */}
+            )}
           </button>
         </div>
         {!this.state.isHide && <div className="expand__content">{this.props.children}</div>}
