8fbec1751d57c482cdd7e29c9369a06333d75f2b
Alex Sedmoy <alexsedmoy@gmail.com>
2022-07-29 22:30:46 +0300

    Expanded Component

--- lesson11/task3/src/Expand.jsx
+++ lesson11/task3/src/Expand.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import 'font-awesome/css/font-awesome.min.css';
+// import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 class Expand extends React.Component {
   constructor(props) {
@@ -23,10 +25,16 @@ class Expand extends React.Component {
           <span className="expand__title">{this.props.title}</span>
           <button className="expand__toggle-btn" onClick={this.handleToggle}>
             {this.state.isHide ? (
+              <i className="fas fa-chevron-down" />
+            ) : (
+              <i className="fas fa-chevron-up" />
+            )}
+
+            {/* {this.state.isHide ? (
               <FontAwesomeIcon icon={faChevronDown} />
             ) : (
               <FontAwesomeIcon icon={faChevronUp} />
-            )}
+            )} */}
           </button>
         </div>
         {!this.state.isHide && <div className="expand__content">{this.props.children}</div>}
