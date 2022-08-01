a65410affb473aec95b7fd62919eccee9870d5c7
Alex Sedmoy <alexsedmoy@gmail.com>
2022-08-02 00:17:39 +0300

    User data - React Hooks

---
+++ lesson14/task2/src/index.jsx
@@ -0,0 +1,10 @@
+// User data - React Hooks
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.scss';
+
+const root = createRoot(document.querySelector('#root'));
+
+root.render(<App />);
