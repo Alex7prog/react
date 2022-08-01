b3c88544dc2c28d9ecea0fa9a08432947523d63a
Alex Sedmoy <alexsedmoy@gmail.com>
2022-08-02 00:24:58 +0300

    Window dimensions - React hooks

---
+++ lesson14/task3/src/index.jsx
@@ -0,0 +1,10 @@
+// Window dimensions - React hooks
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Dimensions from './Dimensions';
+import './index.scss';
+
+const root = createRoot(document.querySelector('#root'));
+
+root.render(<Dimensions />);
