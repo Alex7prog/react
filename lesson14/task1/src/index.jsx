6aeebf6699853203422120e1c641734fe7a2262d
Alex Sedmoy <alexsedmoy@gmail.com>
2022-08-02 00:07:59 +0300

    Counter - React Hooks

---
+++ lesson14/task1/src/index.jsx
@@ -0,0 +1,10 @@
+// Counter - React Hooks
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Counter from './Counter';
+import './index.scss';
+
+const root = createRoot(document.querySelector('#root'));
+
+root.render(<Counter />);
