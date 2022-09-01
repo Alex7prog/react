f5da512a6cab6a3d81933c35a9ccbdacef6196bd
Alex Sedmoy <alexsedmoy@gmail.com>
2022-09-01 15:49:17 +0300

    Component for authorization

--- lesson6/task5/src/Auth.jsx
+++ lesson6/task5/src/Auth.jsx
@@ -1,50 +1,80 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Login from './Login';
 import Logout from './Logout';
 import Spinner from './Spinner';
 
-class Auth extends Component {
-  state = {
-    loginStatus: 'logout',
-  };
+const Auth = () => {
+  const [loginStatus, setLoginStatus] = useState('logout');
 
-  handleLogin = () => {
-    this.setState({
-      loginStatus: 'load',
-    });
+  const handleLogin = () => {
+    setLoginStatus('load');
 
     setTimeout(() => {
-      this.setState({
-        loginStatus: 'login',
-      });
+      setLoginStatus('login');
     }, 2000);
   };
 
-  handleLogout = () => {
-    this.setState({
-      loginStatus: 'logout',
-    });
+  const handleLogout = () => {
+    setLoginStatus('logout');
   };
 
-  render() {
-    if (this.state.loginStatus === 'load') {
-      return (
-        <div className="status">
-          <Spinner size={35} />
-        </div>
-      );
-    }
-
-    return (
-      <div className="status">
-        {this.state.loginStatus === 'logout' ? (
-          <Login onLogin={this.handleLogin} />
-        ) : (
-          <Logout onLogout={this.handleLogout} />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="status">
+      {loginStatus === 'logout' ? (
+        <Login onLogin={handleLogin} />
+      ) : loginStatus === 'load' ? (
+        <Spinner size={35} />
+      ) : (
+        <Logout onLogout={handleLogout} />
+      )}
+    </div>
+  );
+};
 
 export default Auth;
+
+// option - class component
+
+// class Auth extends Component {
+//   state = {
+//     loginStatus: 'logout',
+//   };
+
+//   handleLogin = () => {
+//     this.setState({
+//       loginStatus: 'load',
+//     });
+
+//     setTimeout(() => {
+//       this.setState({
+//         loginStatus: 'login',
+//       });
+//     }, 2000);
+//   };
+
+//   handleLogout = () => {
+//     this.setState({
+//       loginStatus: 'logout',
+//     });
+//   };
+
+//   render() {
+//     if (this.state.loginStatus === 'load') {
+//       return (
+//         <div className="status">
+//           <Spinner size={35} />
+//         </div>
+//       );
+//     }
+
+//     return (
+//       <div className="status">
+//         {this.state.loginStatus === 'logout' ? (
+//           <Login onLogin={this.handleLogin} />
+//         ) : (
+//           <Logout onLogout={this.handleLogout} />
+//         )}
+//       </div>
+//     );
+//   }
+// }
